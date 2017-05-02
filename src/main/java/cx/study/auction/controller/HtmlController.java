6123e038bc8b1afb6fd9d02c3297779ca5d2d93d
yiyayiyayoaaa <18272175681@163.com>
2017-05-02 19:17:55 +0800

    初始化项目结构

---
+++ src/main/java/cx/study/auction/controller/HtmlController.java
@@ -0,0 +1,40 @@
+package cx.study.auction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * Created by chengxiao on 2017/4/30.
+ */
+@Controller
+public class HtmlController {
+
+    @GetMapping(value = "/index")
+    public ModelAndView toIndex(){
+        return new ModelAndView("index");
+    }
+
+    @GetMapping(value = "/login")
+    public ModelAndView toLogin(){
+        return new ModelAndView("login");
+    }
+
+    @GetMapping(value = "/welcome")
+    public ModelAndView toWelcome(){
+        return new ModelAndView("welcome");
+    }
+
+    @GetMapping(value = "/auction-list")
+    public ModelAndView toAuctionList(){
+        return new ModelAndView("auction-list");
+    }
+    @GetMapping(value = "/auction-category")
+    public ModelAndView toAuctionCategory(){
+        return new ModelAndView("auction-category");
+    }
+}
