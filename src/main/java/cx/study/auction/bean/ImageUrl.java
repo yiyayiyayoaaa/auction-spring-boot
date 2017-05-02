70dc356a7e00aab947e3e450ab88155335f9b33e
yiyayiyayoaaa <18272175681@163.com>
2017-05-02 19:18:37 +0800

    初始化项目结构

---
+++ src/main/java/cx/study/auction/bean/ImageUrl.java
@@ -0,0 +1,43 @@
+package cx.study.auction.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * Created by chengxiao on 2017/5/1.
+ */
+@Entity
+public class ImageUrl {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String url;
+    @ManyToOne
+    private Auction auction;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Auction getAuction() {
+        return auction;
+    }
+
+    public void setAuction(Auction auction) {
+        this.auction = auction;
+    }
+}
